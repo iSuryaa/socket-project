9c7f405e71a4b720871e4d6c6a33fb5b0f63ddf1
iSuryaa <suryaprakash2461@gmail.com>
2023-06-09 13:13:18 +0530

    project added

---
+++ Server/server.js
@@ -0,0 +1,36 @@
+const express = require("express");
+const app = express();
+const http = require("http");
+const { Server } = require("socket.io");
+const cors = require("cors");
+
+app.use(cors());
+
+const server = http.createServer(app);
+
+const io = new Server(server, {
+  cors: {
+    origin: "http://localhost:3000",
+    methods: ["GET", "POST"],
+  },
+});
+
+io.on("connection", (socket) => {
+  console.log(`User Connected:${socket.id}`);
+
+  socket.on("join_room", (data) => {
+    socket.join(data);
+    console.log(`User with ID :${socket.id} Joined room :${data} `);
+  });
+
+  socket.on("send_message", (data) => {
+    socket.to(data.roomName).emit("receive_message", data);
+  });
+  socket.on("disconnect", () => {
+    console.log("User Disconnected", socket.id);
+  });
+});
+
+server.listen(3001, () => {
+  console.log("your server is running in 3001");
+});
---
+++ index.js
@@ -0,0 +1,15 @@
+const input = [5, 7, 9, 11, 15, 17];
+const output = 13;
+
+ for (i = 0; i < input.length; i++) {
+   let current = input[i];
+
+   let next = current + 2; 
+    if (input[i + 1] !== next) {
+      console.log(next);
+      break;
+    }
+ }
+
+
+
